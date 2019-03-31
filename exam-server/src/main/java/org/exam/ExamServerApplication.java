a0995a8dac46597bbfcdcfbb18e5562d7a9ae540
shiyuan2he <shiyuan4work@126.com>
2019-03-31 11:48:55 +0800

    swagger添加

--- exam-server/src/main/java/org/exam/ExamServerApplication.java
+++ exam-server/src/main/java/org/exam/ExamServerApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author heshiyuan
@@ -11,6 +12,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @MapperScan("org.exam.mapper")
 //@EnableCaching
+@EnableSwagger2
 public class ExamServerApplication {
 
 	public static void main(String[] args) {
--- exam-server/src/main/java/org/exam/config/Swagger2Config.java
+++ exam-server/src/main/java/org/exam/config/Swagger2Config.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ResponseBody;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -23,16 +24,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
+@Component
 @Configuration
-@EnableSwagger2
 public class Swagger2Config {
     @Value("${swagger2.enable}")
     private boolean enableSwagger;
     @Value("${swagger2.host}")
-    private String swaggerhost;
+    private String swaggerHost;
     @Bean
     public Docket createRestApi() {
-        Predicate<RequestHandler> predicate = new Predicate<RequestHandler>() {
+        /*Predicate<RequestHandler> predicate = new Predicate<RequestHandler>() {
             @Override
             public boolean apply(RequestHandler input) {
                 Class<?> declaringClass = input.declaringClass();
@@ -55,7 +56,7 @@ public class Swagger2Config {
                 return false;
             }
         };
-/*        List<Parameter> pars = new ArrayList<Parameter>();
+        List<Parameter> pars = new ArrayList<>();
         ParameterBuilder ticketPar = new ParameterBuilder();
         //header中的ticket参数非必填，传空也可以
         ticketPar
@@ -63,62 +64,62 @@ public class Swagger2Config {
                 .description("用户token")
                 .defaultValue("eyJhbGciOiJIUzUxMwIjw")
                 .modelRef(new ModelRef("string")).parameterType("header")
-                .required(false).build();
+                .required(false)
+                .build();
 
         //根据每个方法名也知道当前方法在设置什么参数
         pars.add(ticketPar.build());*/
 
         return new Docket(DocumentationType.SWAGGER_2)
-                .host(swaggerhost)
+                .host(swaggerHost)
                 .apiInfo(apiInfo())
                 .select()
                 //为当前包路径
                 .apis(RequestHandlerSelectors.basePackage("org.exam.controller"))
-                .apis(predicate)
+//                .apis(predicate)
                 .paths(PathSelectors.any())
                 //.paths(PathSelectors.ant("/user/**"))
                 .build()
-                //.securitySchemes(securitySchemes())
-                .securityContexts(securityContexts())
-                .pathMapping("/")
-                //.globalOperationParameters(pars)
+//                .securityContexts(securityContexts())
+//                .pathMapping("/")
+//                .globalOperationParameters(pars)
                 .enable(enableSwagger);
     }
 
 
-    private List<ApiKey> securitySchemes() {
+ /*   private List<ApiKey> securitySchemes() {
         return newArrayList(
             new ApiKey("access-token", "access-token", "header")
         );
-    }
+    }*/
 
-    private List<SecurityContext> securityContexts() {
+ /*   private List<SecurityContext> securityContexts() {
         return newArrayList(
                 SecurityContext.builder()
                         .securityReferences(defaultAuth())
                         .forPaths(PathSelectors.regex("^(?!test).*$"))
                         .build()
         );
-    }
+    }*/
 
-    List<SecurityReference> defaultAuth() {
+/*    List<SecurityReference> defaultAuth() {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[3];
         authorizationScopes[0] = authorizationScope;
         return newArrayList(
                 new SecurityReference("access-token", authorizationScopes)
         );
-    }
+    }*/
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 //页面标题
-                .title("微考试系统 RESTful API")
+                .title("微考试系统 restful api")
                 //创建人
-                //.contact(new Contact("Chris", "http://192.168.1.56/development/doc", ""))
+                .contact(new Contact("heshiyuan", "https://github.com/shiyuan2he", "shiyuan4work@126.com"))
                 //版本号
-                .version("1.0")
+                .version("2.0")
                 //描述
-                //.description("API 描述")
+                .description("在线考试系统api")
                 .build();
     }
 }
